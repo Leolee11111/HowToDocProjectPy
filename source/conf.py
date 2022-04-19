d92687ef1497fbeb8f1b33cc32de33229acafb88
Levi <zuxian.li@dorabot.com>
2022-04-19 15:55:10 +0800

    test05

--- source/conf.py
+++ source/conf.py
@@ -69,7 +69,7 @@ exclude_patterns = []
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_theme = "pydata_sphinx_theme"
-html_theme_path = ["https://github.com/pydata/pydata-sphinx-theme"]
+html_theme_path = ['/home/bot/anaconda3/lib/python3.9/site-packages']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
