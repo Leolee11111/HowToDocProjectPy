bdf7df1ad25cca07c9171283f0074e5a06f098e9
Levi <zuxian.li@dorabot.com>
2022-04-19 15:49:50 +0800

    test03

--- source/conf.py
+++ source/conf.py
@@ -69,6 +69,7 @@ exclude_patterns = []
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_theme = "pydata_sphinx_theme"
+html_theme_path = ["https://github.com/pydata/pydata-sphinx-theme"]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
